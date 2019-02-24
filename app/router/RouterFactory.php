fbe2df12815bdba15521937c58de3e1328f57aae
sspu-opava <itworkshop@sspu-opava.cz>
2019-02-24 10:59:04 +0100

    Rozdělení na moduly Admin a Front

---
+++ app/router/RouterFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Nette;
+use Nette\Application\Routers\Route;
+use Nette\Application\Routers\RouteList;
+
+
+final class RouterFactory
+{
+	use Nette\StaticClass;
+
+	/**
+	 * @return Nette\Application\IRouter
+	 */
+	public static function createRouter()
+	{
+		$router = new RouteList();
+        $router[] = $admin = new RouteList('Admin');
+        $admin[] = new Route('admin/<presenter>/<action>[/<id>]', 'Homepage:default');
+
+        $router[] = $front = new RouteList('Front');
+        $front[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+
+		return $router;
+	}
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+IntroNette Sandbox - SŠPU Opava
+===============================
+
+Upravený Nette Sandbox s moduly Admin a Front
