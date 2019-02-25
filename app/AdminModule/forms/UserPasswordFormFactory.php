26d4ca63c9f4675f73994b339939065e4143c6dd
sspu-opava <itworkshop@sspu-opava.cz>
2019-02-25 23:16:48 +0100

    Změna hesla, seznam uživatelů

---
+++ app/AdminModule/forms/UserPasswordFormFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\AdminModule\Forms;
+
+use App\Model;
+use Nette;
+use Nette\Application\UI\Form;
+use Tracy\Debugger;
+
+
+final class UserPasswordFormFactory
+{
+	use Nette\SmartObject;
+
+	const PASSWORD_MIN_LENGTH = 7;
+
+	/** @var FormFactory */
+	private $factory;
+
+	/** @var Model\UserManager */
+	private $userManager;
+
+
+	public function __construct(FormFactory $factory, Model\UserManager $userManager)
+	{
+		$this->factory = $factory;
+		$this->userManager = $userManager;
+	}
+
+
+	/**
+	 * @return Form
+	 */
+	public function create(callable $onSuccess)
+	{
+		$form = $this->factory->create();
+		$form->addHidden('id');
+
+		$passwordInput = $form->addPassword('password', 'Heslo:')
+			->setOption('description', sprintf('minimálně %d znaků', self::PASSWORD_MIN_LENGTH))
+			->setRequired('Zadejte, prosím, heslo')
+			->addRule($form::MIN_LENGTH, null, self::PASSWORD_MIN_LENGTH);
+			
+		$form->addPassword('password2', 'Zopakujte heslo')
+			->setRequired('Zopakujte, prosím, heslo kvůli ověření')
+			->addRule($form::EQUAL, 'Zadaná hesla se neshodují!', $passwordInput);
+		
+		$form->addSubmit('send', 'Změnit heslo');
+
+		$form->onSuccess[] = function (Form $form, $values) use ($onSuccess) {
+			Debugger::barDump($values);
+			$this->userManager->changePassword($values['id'], $values['password']);
+			$onSuccess();
+		};
+
+		return $form;
+	}
+}
